df6ff663d685558d4b90baa1bceac62676924bee
AzureAD\BillyAndersonMatjik <billy.matjik@anteraja.id>
2022-10-08 00:05:22 +0700

    Add Animated VIsibility in header title

--- app/src/main/java/id/bmatjik/todolistcompose/MainActivity.kt
+++ app/src/main/java/id/bmatjik/todolistcompose/MainActivity.kt
@@ -3,13 +3,15 @@ package id.bmatjik.todolistcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -44,8 +46,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HomeScreen() {
-    Column(modifier = Modifier.fillMaxSize()) {
-        Text(text = "Todo", style = MaterialTheme.typography.titleLarge, fontSize = 30.sp)
+    var isMadeByTextVisible by remember { mutableStateOf(false) }
+    Column(modifier = Modifier.fillMaxSize().padding(horizontal = 6.dp)) {
+
+        Text(text = "Todo", style = MaterialTheme.typography.headlineMedium, fontSize = 30.sp, modifier = Modifier.clickable {
+            isMadeByTextVisible = !isMadeByTextVisible
+        })
+        AnimatedVisibility(visible = isMadeByTextVisible) {
+            Text(text = "Made By BAndersonMatjik", style = MaterialTheme.typography.headlineSmall, fontSize = 14.sp)
+        }
         LazyColumn(content = {
 
         })
@@ -55,7 +64,9 @@ fun HomeScreen() {
 @Composable
 fun TodoItem(todo: Todo){
     Card(shape = RoundedCornerShape(20.dp)){
-        Column(modifier = Modifier.fillMaxWidth().padding(6.dp)) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(6.dp)) {
             Row() {
                 Text(text = todo.title, style = MaterialTheme.typography.titleSmall)
                 Spacer(Modifier.weight(1f))
